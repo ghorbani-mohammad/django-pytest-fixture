d86bc7073e81f2155871eb6c081779767f319b63
Mohammad <mohammad.ghorbani@hotmail.com>
2022-10-20 12:29:46 +0330

    add test_should_check_password add test_should_not_check_unusable_password

--- project/tests/test_user_auth.py
+++ project/tests/test_user_auth.py
@@ -3,3 +3,14 @@ from django.contrib.auth.models import User
 def test_should_create_user_with_username(db) -> None:
     user = User.objects.create_user("Haki")
     assert user.username == "Haki"
+
+def test_should_check_password(db) -> None:
+    user = User.objects.create_user("A")
+    user.set_password("secret")
+    assert user.check_password("secret") is True
+
+def test_should_not_check_unusable_password(db) -> None:
+    user = User.objects.create_user("A")
+    user.set_password("secret")
+    user.set_unusable_password()
+    assert user.check_password("secret") is False

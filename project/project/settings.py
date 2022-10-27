7b896f1c1570c82836cf3569d7795774e588a748
Mohammad <mohammad.ghorbani@hotmail.com>
2022-10-27 18:38:20 +0330

    minor change

--- project/project/settings.py
+++ project/project/settings.py
@@ -3,10 +3,6 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "django-insecure-k69)x4eqqk3=-j)o2p=w8prtgs!c8k9t&e#nmlbzrjrpxm5bi#"
 
 # SECURITY WARNING: don't run with debug turned on in production!
